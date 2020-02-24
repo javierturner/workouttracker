c244389b45a42b6e9d0e31b11abbacc7f17a3a36
JT Turner <j.tturner14@yahoo.com>
2020-02-24 18:01:37 -0500

    added requires and html routes

---
+++ server.js
@@ -0,0 +1,32 @@
+//Required
+const express = require ('express');
+const logger = require ('morgan');
+const mongoose = require ('mongoose');
+const path = require ('path');
+
+const PORT = process.env.PORT || 3000;
+const db =require ('./models');
+const app = express();
+
+app.use(logger('dev'));
+app.use (express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.end.MONGODB_URI || 'mongodb://localhost/budget', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//HTML Routes
+app.get('/stats', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/stats.html'))
+});
+
+app.get('/exercise', (res,res) => {
+    res.sendFile(path.join(__dirname, '/public/exercise.html'));
+});
+
+app.get('/', (res,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
