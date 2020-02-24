6f5e13566b00488c6be72cd2160406c3d1d25055
JT Turner <j.tturner14@yahoo.com>
2020-02-24 18:06:03 -0500

    added API routes

--- server.js
+++ server.js
@@ -23,10 +23,41 @@ app.get('/stats', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/stats.html'))
 });
 
-app.get('/exercise', (res,res) => {
+app.get('/exercise', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/exercise.html'));
 });
 
-app.get('/', (res,res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+
+//API Routes
+app.post('/api/workouts', ({body}, res) => {
+    db.Workout.create(body)
+    .then(workouts => res.json(workouts))
+});
+
+app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find()
+        .then(workouts => res.json(workouts))
+        .catch(err => res.json(err))
+});
+
+app.get('/api/workouts', (req, res) => {
+    db.Workout.find()
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err))
+});
+
+app.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, { 
+        $push: {exercises: req.body}
+    })
+    .then(workouts => res.json(workouts))
+    .catch(err=> res.json(err))
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
