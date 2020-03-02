a084073dc88b053dab17d52440c203065cea0b0f
JT Turner <j.tturner14@yahoo.com>
2020-03-02 18:55:45 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use (express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
